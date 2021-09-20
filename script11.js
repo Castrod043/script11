8b95e13fb1d440cde9d2406df318f8d9d0eb738d
Castrod043 <64606385+Castrod043@users.noreply.github.com>
2021-09-20 15:05:22 +0200

    Add files via upload

---
+++ script11.js
@@ -0,0 +1,20 @@
+// iterators
+
+
+//Oude stijl 
+
+const colorArray = ["yellow", "blue", "Red", "orange"];
+
+let i = 0;
+
+while (i < colorArray.length) {
+    console.log(colorArray[i]);
+    i++;
+}
+
+//for versie 
+
+for (let i = 0; i < colorArray.length; i++) {
+    console.log(colorArray[i]);
+
+}
